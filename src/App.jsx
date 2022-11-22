a96262993f303c0607f1b088dc12ecc2f4fa9e0d
hy-reza <hy.rezaalfanda@gmail.com>
2022-11-22 11:45:25 +0700

    Add Auth Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { Routes, Route } from "react-router-dom";
-import { Home, Flight } from "./pages";
+import { Home, Flight, Auth } from "./pages";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/flight" element={<Flight />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 };
