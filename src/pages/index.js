09d972d556133c81c460859da2256564d8b9ae16
Damas <muhammaddamas81@gmail.com>
2022-11-30 18:03:53 +0700

    add import SearchFlight di index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Flight from "./Flight";
 import Home from "./Home";
 import Auth from "./Auth";
+import SearchFlight from "./SearchFlight";
 
-export { Flight, Home, Auth };
+export { Flight, Home, Auth, SearchFlight };
