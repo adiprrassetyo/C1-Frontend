4ef9afe42ce8514cb4dbdd0da47a4e92a3e1eed1
Damas <muhammaddamas81@gmail.com>
2022-11-30 18:15:15 +0700

    add route path search-flight

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/styles/main.css"
 import { Routes, Route } from "react-router-dom";
-import { Home, Flight, Auth } from "./pages";
+import { Home, Flight, Auth, SearchFlight } from "./pages";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/flight" element={<Flight />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/search-flight" element={<SearchFlight />} />
     </Routes>
   );
 };
