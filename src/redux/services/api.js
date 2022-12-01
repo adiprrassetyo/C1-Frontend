5858003452a83ee7d0889137c4cc678a2674f213
hy-reza <hy.rezaalfanda@gmail.com>
2022-12-01 18:09:59 +0700

    configure redux store provider

--- src/redux/services/api.js
+++ src/redux/services/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://raw.githubusercontent.com/fnurhidayat/probable-garbanzo/main/data",
+  baseURL: "https://binair-backend-production.up.railway.app/api/v1",
 });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("user")).token
-    }`;
+    req.headers.Authorization = `Bearer ${JSON.parse(
+      localStorage.getItem("user").token
+    )}`;
   }
   return req;
-}); 
+});
--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,2 +1,100 @@
-import * as cars from "../services/carsService";
+import * as auth from "../services/authService";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const registerUser = createAsyncThunk("user/register", async (user, navigate) => {
+  navigate('')
+  try {
+    const res = await auth.register(user);
+    if(res.data.status == "success"){
+      navigate('/auth')
+    }
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+export const loginUser = createAsyncThunk("user/login", async (user, navigate) => {
+  try {
+    const res = await auth.login(user);
+    if(res.data.status == "success"){
+      navigate('/')
+    }
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+export const logoutUser = createAsyncThunk("user/logout", async (navigate) => {
+  try {
+    const res = await auth.logout();
+    if(res.data.status == "success"){
+      navigate('/')
+    }
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: false,
+    message: "",
+    user: {},
+  },
+  reducers: {},
+  extraReducers: {
+    //register
+    [registerUser.pending]: (state, action) => {
+      return { ...state, loading: true };
+    },
+    [registerUser.fulfilled]: (state, action) => {
+      return {
+        loading: false,
+        message: action.payload.message,
+        user: action.payload.data,
+      };
+    },
+    [registerUser.rejected]: (state, action) => {
+      return { ...state, loading: false };
+    },
+    //login
+    [loginUser.pending]: (state, action) => {
+      return { ...state, loading: true };
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ token: action.payload.data.accessToken })
+      );
+      return {
+        loading: false,
+        message: action.payload.message,
+        user: action.payload.data,
+      };
+    },
+    [loginUser.rejected]: (state, action) => {
+      return { ...state, loading: false };
+    },
+    //logout
+    [logoutUser.pending]: (state, action) => {
+      return { ...state, loading: true };
+    },
+    [logoutUser.fulfilled]: (state, action) => {
+      localStorage.removeItem("user");
+      return {
+        loading: false,
+        message: action.payload.message,
+        user: {},
+      };
+    },
+    [logoutUser.rejected]: (state, action) => {
+      return { ...state, loading: false };
+    },
+  },
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
