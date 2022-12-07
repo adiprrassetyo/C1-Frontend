a0adca7ada2450cc1a28393795443cef1c7270a9
Noviyana <lingnoviyana123@gmail.com>
2022-12-07 14:59:12 +0700

    setting promo page

---
+++ src/pages/Promo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header, Footer } from "../components";
+
+const Promo = () => {
+  return (
+    <div>
+      <Header />
+      Promo
+      <Footer />
+    </div>
+  );
+};
+
+export default Promo;
