10124da2e9700afbc95ccd9dd315ff898954e5f9
hy-reza <hy.rezaalfanda@gmail.com>
2022-12-14 20:25:25 +0700

    Dep

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -77,17 +77,17 @@ const Header = () => {
                     <NavDropdown.Item>US</NavDropdown.Item>
                     <NavDropdown.Item>IDR</NavDropdown.Item>
                   </NavDropdown> */}
-
-                  <Button
-                    href="/auth"
-                    className={
-                      user
-                        ? "btn btn-light button-login d-none"
-                        : "btn btn-light button-login"
-                    }
-                  >
-                    Login
-                  </Button>
+                  <Link to="/#/auth">
+                    <Button
+                      className={
+                        user
+                          ? "btn btn-light button-login d-none"
+                          : "btn btn-light button-login"
+                      }
+                    >
+                      Login
+                    </Button>
+                  </Link>
 
                   <NavDropdown
                     title={
