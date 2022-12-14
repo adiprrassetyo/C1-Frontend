eaed4770e2a00e1f45f0a0bda7ab02ff16806fd2
hy-reza <hy.rezaalfanda@gmail.com>
2022-12-14 19:52:14 +0700

    add hash router

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "remixicon/fonts/remixicon.css";
 import "./assets/styles/main.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Protected from "./utils/Protected";
 import {
   Home,
   Flight,
   Auth,
-  NotFound, Tickets,
+  NotFound,
+  Tickets,
   DashboardLayout,
   MainMenu,
   PromosMenu,
@@ -17,26 +18,28 @@ import {
   TransactionsMenu,
   UsersMenu,
   Promo,
-  DetailPromo
+  DetailPromo,
 } from "./pages";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/flight" element={<Flight />}></Route>
-      <Route path="/flight/search" element={<Tickets />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/promo" element={<Promo />} />
-      <Route path="/promo/view/1" element={<DetailPromo />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route path="" element={<MainMenu />} />
-        <Route path="promos" element={<PromosMenu />} />
-        <Route path="tickets" element={<TicketsMenu />} />
-        <Route path="transactions" element={<TransactionsMenu />} />
-        <Route path="users" element={<UsersMenu />} />
-      </Route>
+      <HashRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/flight" element={<Flight />}></Route>
+        <Route path="/flight/search" element={<Tickets />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/promo" element={<Promo />} />
+        <Route path="/promo/view/1" element={<DetailPromo />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="" element={<MainMenu />} />
+          <Route path="promos" element={<PromosMenu />} />
+          <Route path="tickets" element={<TicketsMenu />} />
+          <Route path="transactions" element={<TransactionsMenu />} />
+          <Route path="users" element={<UsersMenu />} />
+        </Route>
+      </HashRouter>
     </Routes>
   );
 };
