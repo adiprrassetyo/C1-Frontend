71990ba3d4e5edf26d31d67586b3336b6961c2d8
Damas <muhammaddamas81@gmail.com>
2022-12-21 18:27:39 +0700

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,8 @@ import Auth from "./Auth";
 import NotFound from "./NotFound";
 import Tickets from "./Tickets";
 import Profile from "./Profile";
+import Password from "./Password";
+import Passenger from "./Passenger";
+import Order from "./Order";
 
-export { Flight, Home, Auth, NotFound, Tickets, Profile};
+export { Flight, Home, Auth, NotFound, Tickets, Profile, Password, Passenger, Order};
