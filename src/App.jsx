3b39178f2282d4f9a90361b256ef8f4b8945aa93
Damas <muhammaddamas81@gmail.com>
2022-12-21 18:29:58 +0700

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import 'remixicon/fonts/remixicon.css'
 import "./assets/styles/main.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, Flight, Auth, NotFound, Tickets, Profile } from "./pages";
+import { Home, Flight, Auth, NotFound, Tickets, Profile, Password, Passenger, Order} from "./pages";
 import Protected from "./utils/Protected";
 
 const App = () => {
@@ -16,6 +16,9 @@ const App = () => {
       <Route path="/tickets" element={<Tickets />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/account/profile" element={<Profile />} />
+      <Route path="/account/password" element={<Password />} />
+      <Route path="/account/passenger" element={<Passenger />} />
+      <Route path="/account/order" element={<Order />} />
     </Routes>
   );
 };
