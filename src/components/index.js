d7cf1569b3fb271775f097b977a93be9ab448ec7
Damas <muhammaddamas81@gmail.com>
2022-12-28 20:41:30 +0700

    add HeaderBookingDone di index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeaderBooking from "./HeaderBooking";
+import HeaderBookingDone from "./HeaderBookingDone";
 
-export { Header, Footer, HeaderBooking };
+export { Header, Footer, HeaderBooking, HeaderBookingDone };
