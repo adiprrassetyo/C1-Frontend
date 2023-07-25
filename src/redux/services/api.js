cdf8d9665c383e5ba9817a08c77c4aff47083e54
adiprrassetyo <adiprstyo11@gmail.com>
2023-07-25 10:38:07 +0700

    change base url

--- src/redux/services/api.js
+++ src/redux/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://binair-backend-production.up.railway.app/api/v1',
+    baseURL: 'https://c1-backend-production.up.railway.app/api/v1',
 })
 
 API.interceptors.request.use((req) => {
